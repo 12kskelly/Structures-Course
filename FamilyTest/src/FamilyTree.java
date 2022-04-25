e406f3fc929219d758f627b69e97a9140c6274a8
12kskelly <12kskelly@gmail.com>
2022-04-25 21:26:34 +0100

    Adding on the add child method which will add a child to the main ancestor and their partner

--- FamilyTest/src/FamilyTree.java
+++ FamilyTest/src/FamilyTree.java
@@ -24,6 +24,21 @@ public class FamilyTree {
         familyMember.name = name;
         familyMember.partner = this.current;
         this.current.partner = familyMember;
-        
+    }
+    
+    public void addChild(String name) {
+        FamilyTreeNode familyMember = new FamilyTreeNode();
+        familyMember.name = name;
+        familyMember.ancestor = this.current;
+        familyMember.partner = this.current.partner;
+        if (this.current.child == null) {
+            this.current.child = familyMember;
+        } else {
+            FamilyTreeNode next = this.current.child;
+            while (next.sibling != null) {
+                next = next.sibling;
+            }
+            next.sibling = familyMember;
+        }
     }
 }
