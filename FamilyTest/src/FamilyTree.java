456cbffa1b409fbbb3e4fac8e37d776d8b2c8f3a
12kskelly <12kskelly@gmail.com>
2022-04-25 20:56:03 +0100

    Setting up the Family Tree node and its variables

--- FamilyTest/src/FamilyTree.java
+++ FamilyTest/src/FamilyTree.java
@@ -1,4 +1,16 @@
 
 public class FamilyTree {
     
+    private class FamilyTreeNode {
+        private String name;
+        private FamilyTreeNode ancestor;
+        private FamilyTreeNode partner;
+        private FamilyTreeNode sibling;
+        private FamilyTreeNode child;
+    }
+    
+    private FamilyTreeNode manager;
+    private FamilyTreeNode current;
+    private FamilyTreeNode current2;
+    
 }
