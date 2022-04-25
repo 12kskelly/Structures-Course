0f9703c5b8df1695a4e6ce2e18224f74aacefee4
12kskelly <12kskelly@gmail.com>
2022-04-25 20:44:00 +0100

    Adding on menu options text and menus options

--- FamilyTest/src/FamilyTest.java
+++ FamilyTest/src/FamilyTest.java
@@ -1,11 +1,30 @@
 
 public class FamilyTest {
-  
+
     public static void main(String[] args) {
         String name = Input.getString("Input the ancestor's name: ");
         FamilyTree familytree = new FamilyTree(name);
         name = Input.getString("What is their partner's name: ");
         familytree.addPartner(name);
         Integer option;
-    
+        do {
+            System.out.println("0: quit");
+            System.out.println("1: add child to current");
+            System.out.println("2: find family member");
+            System.out.println("3: display family");
+            option = Input.getInteger("input option: ");
+            switch (option) {
+                case 1:
+                    System.out.println("Test 1");
+                    break;  
+                case 2:
+                    System.out.println("Test 2");
+                    break;
+                case 3:
+                    System.out.println("Test 3");
+                    break;             
+            }
+        } while (option != 0);
+    }
+
 }
