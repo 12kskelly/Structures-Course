a3368d22ff116a5805595a22b9ee13a0b7704f10
12kskelly <12kskelly@gmail.com>
2022-04-26 16:06:40 +0100

    Touching up code and adding on extra menu option

--- FamilyTest - Part 2/src/FamilyTreeTest.java
+++ FamilyTest - Part 2/src/FamilyTreeTest.java
@@ -7,20 +7,21 @@ public class FamilyTreeTest {
         Integer option = 7;
         do {
             try{
-                System.out.println("0: quit");
-                System.out.println("1: add child to current");
-                System.out.println("2: add partner to current");
-                System.out.println("3: display family");
-                option = Input.getInteger("input option: ");
+                System.out.println("0: Quit");
+                System.out.println("1: Add child to current");
+                System.out.println("2: Add partner to current");
+                System.out.println("3: Display family");
+                System.out.println("4: Find family member");
+                option = Input.getInteger("Input Option: ");
                 switch (option) {
                     case 1:
                         try{
                             familytree.checkPartner(name);
                             name = Input.getString("What is the child's name?: ");
                             familytree.addChild(name);
-                            System.out.println("Child Added\n");
+                            System.out.println(familytree);
                         } catch(FamilyTree.PartnerException e){
-                            System.out.println("There has to be two parent to a child");
+                            System.out.println("There has to be two parents to a child\n");
                         }
                         break;
                     case 2:
@@ -28,8 +29,9 @@ public class FamilyTreeTest {
                             familytree.checkPartner(name);
                             System.out.println("There is already a partner for this person\n");
                         } catch(FamilyTree.PartnerException e){
-                            name = Input.getString("What is the partner's name?: ");
+                            name = Input.getString("What is the partner's name?: \n");
                             familytree.addPartner(name);
+                            System.out.println(familytree);
                         }
                         break;
                      case 3:
@@ -41,7 +43,7 @@ public class FamilyTreeTest {
                 System.out.println("Please choose a valid menu option between 0 and 3 \n");
             }
         }while (option != 0);
-        System.out.println("Thanks for visiting.");
+        System.out.println("\nThanks for visiting.");
         }
 
     }
